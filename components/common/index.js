dc2014887259f6124d42d8095b970142da3196d4
tangxka <tangxkwork@163.com>
2020-08-25 15:14:30 +0800

    加入lable

---
+++ components/common/index.js
@@ -0,0 +1,5 @@
+import Label from './svelte/Label.svelte';
+import Button from './svelte/Button.svelte';
+import A from './svelte/A.svelte';
+
+export {Label, Button, A};
---
+++ components/common/svelte/Label.svelte
@@ -0,0 +1,28 @@
+<span
+  use:useActions={use}
+  use:forwardEvents
+  class="
+    {className}
+    {context === 'button' ? 'mdc-button__label' : ''}
+    {context === 'fab' ? 'mdc-fab__label' : ''}
+    {context === 'chip' ? 'mdc-chip__text' : ''}
+    {context === 'tab' ? 'mdc-tab__text-label' : ''}
+    {context === 'image-list' ? 'mdc-image-list__label' : ''}
+    {context === 'snackbar' ? 'mdc-snackbar__label' : ''}
+  "
+  {...((context === 'snackbar') ? {role: 'status', 'aria-live': 'polite'} : {})}
+  {...exclude($$props, ['use', 'class'])}
+><slot></slot></span>
+
+<script>
+  import {getContext} from 'svelte';
+  import {get_current_component} from 'svelte/internal';
+  import {forwardEventsBuilder} from './../js/forwardEvents';
+  import {exclude} from './../js/exclude.js';
+  import {useActions} from './../js/useActions.js';
+  const forwardEvents = forwardEventsBuilder(get_current_component());
+  export let use = [];
+  let className = '';
+  export {className as class};
+  const context = getContext('SMUI:label:context');
+</script>
